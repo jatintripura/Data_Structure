1c29007c39518e870744b661328967fe242086b7
jatintripura <jatintripura49@gmail.com>
2023-07-17 20:29:39 +0600

    Sum Arra algorithms push

---
+++ 03_Sum_Array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    int array[n];
+    printf("Enter the elements of array: ");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum+=array[i];
+    }
+    printf("The sum of elements of array is: %d\n",sum);
+    return 0;
+}
