cef86dad3a37f385f0315db6a6d8214cac98d27c
jatintripura <jatintripura49@gmail.com>
2023-07-17 20:46:49 +0600

    Linear search

---
+++ 09_a_Linear_search.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,index=-1;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    int array[n];
+    printf("Enter the elements of array: ");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    int search;
+    printf("Enter the element to be searched: ");
+    scanf("%d",&search);
+    for(int i=0;i<n;i++)
+    {
+        if(array[i]==search){
+            printf("%d\n",array[i]);
+            index=1;
+        }
+    }
+    if(index==-1)
+     printf("Element not found");
+    
+
+    return 0;
+}
